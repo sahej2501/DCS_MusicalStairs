e0f6231fcc995c27da0864f65b97c28424b2f330
sahej2501 <sahej2501@gmail.com>
2021-10-04 22:54:12 +0100

    3proto.py has been worked on, not a viable solution yet

--- LaserRND/3proto.py
+++ LaserRND/3proto.py
@@ -6,30 +6,19 @@ import vlc
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-#creating a new vlc instance
-player = vlc.Instance()
-player2 = vlc.Instance()  
-# creating a new media list
-media_list = player.media_list_new()
-media_list2 = player.media_list_new()
-# creating a media player object
-media_player = player.media_list_player_new()
- 
-# creating a new media
-step1 = player.media_new("piano_import_C.mp3")
-step2 = player2.media_new("piano_import_F.mp3")  
-# adding media to media list
-media_list.add_media(step1)
-media_list.add_media(step2)  
-# setting media list to the mediaplayer
-media_player.set_media_list(media_list)
-  
-# setting loop
-player.vlm_set_loop("step1", True)
-
-
-#define the pin that goes to the circuit
 pin_to_circuit = 10
+
+
+#fuction makes vlc instances for each step
+def vlc_instance( fname):
+    player = vlc.Instance()
+    media_list = player.media_list_new()
+    media_player = player.media_list_player_new()
+    step = player.media_new(fname)
+    media_list.add_media(step)
+    media_player.set_media_list(media_list)
+    player.vlm_set_loop("step", True)
+ 
 
 def rc_time (pin_to_circuit):
     count = 0
